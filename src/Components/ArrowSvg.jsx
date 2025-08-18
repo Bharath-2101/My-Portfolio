84ea1c99902fc229f54a77b931e591c881e53806
Bharath <bharthkamatham143@gmail.com>
2025-08-18 16:17:48 +0530

    Added a new Variant of Directional Arrow

--- src/Components/ArrowSvg.jsx
+++ src/Components/ArrowSvg.jsx
@@ -1,13 +1,38 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import React from "react";
 
 const ArrowSvg = () => {
+  useGSAP(() => {
+    gsap.to(".rects", {
+      opacity: 0,
+      ease: "cubic-bezier(0.76, 0, 0.24, 1)",
+      stagger: 0.2,
+      duration: 1,
+      repeat: -1,
+      yoyo: true,
+    });
+  }, []);
   return (
-    <svg viewBox="0 0 50 50" fill="none" className=" h-[20px] w-[20px]">
-      <path
-        d="M0 0 L48 48 M48 10 L48 48 L10 48"
-        stroke="white"
-        strokeWidth={3}
-      />
+    <svg
+      viewBox="0 0 24 21"
+      fill="none"
+      className=" h-[40px] w-[40px] rotate-45"
+    >
+      <rect y="9" width="3" className="rects" height="3" fill="white" />
+      <rect x="3" y="9" width="3" className="rects" height="3" fill="white" />
+      <rect x="6" y="9" width="3" className="rects" height="3" fill="white" />
+      <rect x="9" y="9" width="3" className="rects" height="3" fill="white" />
+      <rect x="12" y="9" width="3" className="rects" height="3" fill="white" />
+      <rect x="15" y="9" width="3" className="rects" height="3" fill="white" />
+      <rect x="21" y="9" width="3" className="rects" height="3" fill="white" />
+      <rect x="18" y="9" width="3" className="rects" height="3" fill="white" />
+      <rect x="18" y="12" width="3" className="rects" height="3" fill="white" />
+      <rect x="18" y="6" width="3" className="rects" height="3" fill="white" />
+      <rect x="15" y="15" width="3" className="rects" height="3" fill="white" />
+      <rect x="15" y="3" width="3" className="rects" height="3" fill="white" />
+      <rect x="12" y="18" width="3" className="rects" height="3" fill="white" />
+      <rect x="12" width="3" className="rects" height="3" fill="white" />
     </svg>
   );
 };
