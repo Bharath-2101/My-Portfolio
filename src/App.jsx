47be0764f84ab87a1ad4a313d6667b278461d4f3
Bharath <bharthkamatham143@gmail.com>
2025-08-19 12:29:00 +0530

    Added Lenis implementation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import gsap from "gsap";
 import MorphSVGPlugin from "gsap/MorphSVGPlugin";
 import { SplitText } from "gsap/SplitText";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Lenis from "@studio-freight/lenis";
 
 gsap.registerPlugin(MorphSVGPlugin, SplitText, ScrollTrigger);
 
 const App = () => {
+  const isTablet = window.innerWidth > 768;
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 5,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      smoothTouch: isTablet,
+      touchMultiplier: 2,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      lenis.destroy();
+      gsap.ticker.remove((time) => lenis.raf(time * 1000));
+    };
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
