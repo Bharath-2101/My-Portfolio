6f6ae9b9d6859d47cacd826cc5ac2f0864372f8f
Bharath <bharthkamatham143@gmail.com>
2025-08-20 11:35:47 +0530

    Added worksSection into HomePage.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SideMenu from "../Sections/SideMenu";
 import HeroSection from "../Sections/HeroSection";
 import AboutMe from "../Sections/AboutMe";
+import WorksSection from "../Sections/WorksSection";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <SideMenu />
       <HeroSection />
       <AboutMe />
+      <WorksSection />
       <div className="h-[100dvh] w-screen"></div>
     </div>
   );
