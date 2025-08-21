b7b7ff44e8c334a53ee5282139a46fca079dcb46
Bharath <bharthkamatham143@gmail.com>
2025-08-21 08:55:53 +0530

    Finished the work sectionon both mobile and desktop view.

--- src/Sections/WorksSection.jsx
+++ src/Sections/WorksSection.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import styles from "./styles/WorksSection.module.scss";
 import WorksDesktop from "../Components/WorksDesktop";
 import Works from "../Components/Works";
+import HeroSectionCurve from "../Components/HeroSectionCurve";
 
 const WorksSection = () => {
   const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1024);
@@ -19,6 +20,21 @@ const WorksSection = () => {
     <div className={`${styles.MainContainer}`}>
       <div className={`${styles.MainTitle}`}>RECENT WORKS</div>
       {isDesktop ? <WorksDesktop /> : <Works />}
+      <div
+        className="w-full flex justify-center items-center"
+        style={{ marginTop: `calc(clamp(4rem, 4.5vw, 5rem) + 60px)` }}
+      >
+        <div className={styles.AboutMeButton}>
+          <span>
+            ALL WORKS<sup className="opacity-50">11</sup>
+          </span>
+          {[...Array(4)].map((_, i) => (
+            <span key={i} />
+          ))}
+        </div>
+      </div>
+
+      <HeroSectionCurve fill="#FFFFFF" filterColor="rgba(255,255,255,0.7)" />
     </div>
   );
 };
