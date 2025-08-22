4648d0fe08e768d527635d77869c7706f717a057
Bharath <bharthkamatham143@gmail.com>
2025-08-22 11:50:57 +0530

    Added social media links.

--- src/Project Data/SocialMediaLinks.js
+++ src/Project Data/SocialMediaLinks.js
@@ -1,8 +1,8 @@
 const SocialMediaLinks = [
-  { title: "GITHUB" },
-  { title: "LINKEDIN" },
-  { title: "DRIBBLE" },
-  { title: "INSTAGRAM" },
+  { title: "GITHUB", link: "https://github.com/Bharath-2101" },
+  { title: "LINKEDIN", link: "https://www.linkedin.com/in/bharthkumark/" },
+  { title: "DRIBBLE", link: "https://dribbble.com/iamgoodman9" },
+  { title: "INSTAGRAM", link: "https://www.instagram.com/bharathkamatham" },
 ];
 
 export { SocialMediaLinks };
