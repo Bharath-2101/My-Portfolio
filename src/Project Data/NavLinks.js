ec95fc381f55f62827727528124cadb61db352d6
Bharath <bharthkamatham143@gmail.com>
2025-08-22 11:50:19 +0530

    Added Working links of social media

--- src/Project Data/NavLinks.js
+++ src/Project Data/NavLinks.js
@@ -6,9 +6,9 @@ const NavLinks = [
 ];
 
 const SocialMedia = [
-  { title: "RESUME" },
-  { title: "GITHUB" },
-  { title: "LINKEDIN" },
-  { title: "DRIBBLE" },
+  { title: "RESUME", link: "/Resume.pdf" },
+  { title: "GITHUB", link: "https://github.com/Bharath-2101" },
+  { title: "LINKEDIN", link: "https://www.linkedin.com/in/bharthkumark/" },
+  { title: "DRIBBLE", link: "https://dribbble.com/iamgoodman9" },
 ];
 export { NavLinks, SocialMedia };
