9c7e0395524e6d0cae7f1e07459d97faa2d528c6
Bharath <bharthkamatham143@gmail.com>
2025-08-23 09:36:58 +0530

    Imported Projects.

--- src/Project Data/index.js
+++ src/Project Data/index.js
@@ -1,5 +1,13 @@
 import { NavLinks, SocialMedia } from "./NavLinks";
 import { Works, mobileWorks } from "./Works";
 import { SocialMediaLinks } from "./SocialMediaLinks";
+import { projects } from "./Projects";
 
-export { NavLinks, SocialMedia, Works, mobileWorks, SocialMediaLinks };
+export {
+  NavLinks,
+  SocialMedia,
+  Works,
+  mobileWorks,
+  SocialMediaLinks,
+  projects,
+};
